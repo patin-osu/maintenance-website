debfc8c5e83689faeb8ac1a64dea5543c8ba19c7
Patin Muangjan <patinopsu@gmail.com>
2024-01-14 20:00:15 +0700

    Update to minic more like main website

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 window.addEventListener('load', function () {
   const loadingScreen = document.querySelector('.loading-screen');
-  loadingScreen.classList.add('hidden');
+  loadingScreen.classList.add('fade-out');
 });
 
-const parallaxContainer = document.querySelector('.container');
-
-parallaxContainer.addEventListener('mousemove', (e) => {
-    const { clientX, clientY } = e;
-    const { offsetWidth, offsetHeight } = parallaxContainer;
-    const x = (clientX / offsetWidth - 0.5) * 20;
-    const y = (clientY / offsetHeight - 0.5) * 20;
-
-    const parallaxBackground = document.querySelector('.parallax-background');
-    parallaxBackground.style.transform = `translate(${x}px, ${y}px) scale(1.1)`;
-});
